eb02c0a2a8c9fd032eef473cd63db6b6c31f51f4
Твоє Ім'я <vadik-kulinich@mail.ru>
2025-07-03 00:27:44 +0300

    add cookie

--- js/main_script.js
+++ js/main_script.js
@@ -31,3 +31,15 @@ toggleSelectors.forEach(selector => {
     });
 });
 
+  const cookieConsent = document.getElementById('cookie-consent');
+  const acceptBtn = document.getElementById('accept-cookies');
+
+  // Якщо кукі вже прийняті — не показуємо
+  if (!localStorage.getItem('cookieAccepted')) {
+    cookieConsent.style.display = 'block';
+  }
+
+  acceptBtn.addEventListener('click', () => {
+    localStorage.setItem('cookieAccepted', 'true');
+    cookieConsent.style.display = 'none';
+  });
